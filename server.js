97e094ce6b2d34e6e07930b4efe3721c4883690b
Ukolabs40 <moh.tetuko@gmail.com>
2026-01-24 21:22:48 +0700

    fix: ADAMANTIUM rollout - vite in deps, npm start, and lint fixes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 // RADAR VERSION TAG
-const RADAR_VERSION = "2.0.3-TITANIUM";
+const RADAR_VERSION = "2.0.4-ADAMANTIUM";
 console.log(`[SYS] Initializing RADAR ENGINE ${RADAR_VERSION}...`);
 
 // Health check endpoint
